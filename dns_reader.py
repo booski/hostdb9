52de077bf700d19f71ddf72f8407796065b0d469
Erik Thuning <boooink@gmail.com>
2018-06-26 13:14:42 +0200

    Changed the reader to only care about specified networks and/or cnames. Also modified the handling of extra names to include those names as comments in the output.

--- dns_reader.py
+++ dns_reader.py
@@ -1,23 +1,44 @@
 # coding=utf-8
 
-def read(client, print_warnings):
+import sys
+
+def read(client, vlans, cnames, print_warnings):
     lines = []
+    if cnames:
+        for cname in client.list_cnames():
+            lines.append('cname\t' + cname['name'])
+            lines.append('target\t' + cname['canonical'])
+            lines.append('')
     for vlan in client.list_vlans():
         net = vlan['network']
+        if net not in vlans:
+            continue
         lines.append('')
         lines.append('network\t' + net)
         for ip in client.list_vlan_ips(net):
+            types = ip['types']
             addr = ip['ip_address']
             lines.append('')
+            if types and 'HOST' not in types:
+                lines.append('host\t' + addr + '\t# in use as: '+', '.join(types))
+                continue
             lines.append('host\t' + addr)
             names = ip['names']
             name = ''
+            extra = ''
             if len(names) > 0:
                 name = names[0]
-                if len(names) > 1 and print_warnings:
-                    print('Warning! Ignoring additional names for ' + addr)
+                if len(names) > 1:
+                    extra = '\t# additional names: ' + ', '.join(names[1:])
+                    if print_warnings:
+                        print('Warning! '+ addr + ' has several names. '
+                              + 'Adding extra names as file comment.',
+                              file=sys.stderr)
             if name:
-                lines.append('name\t' + name)
+                append_line = 'name\t' + name
+                if extra:
+                    append_line += extra
+                lines.append(append_line)
                 (comment, aliases) = client.get_host_info(name)
                 if comment:
                     lines.append('comment\t' + comment)
@@ -26,8 +47,4 @@ def read(client, print_warnings):
             mac = ip['mac_address']
             if mac and not name.startswith('dhcp'):
                 lines.append('mac\t' + mac)
-    for cname in client.list_cnames():
-        lines.append('')
-        lines.append('cname\t' + cname['name'])
-        lines.append('target\t' + cname['canonical'])
     return lines
